6aa146cc9a439a53bc7fa3779490359e646b668c
yifeng <mellome1992@gmail.com>
2019-08-10 17:41:04 +0200

    mybatis added

--- src/main/java/com/yifeng/springboot/sprintbootdemo/SprintbootDemoApplication.java
+++ src/main/java/com/yifeng/springboot/sprintbootdemo/SprintbootDemoApplication.java
@@ -1,12 +1,15 @@
 package com.yifeng.springboot.sprintbootdemo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.yifeng.springboot.sprintbootdemo.dao")
 public class SprintbootDemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("启动 Spring Boot...");
         SpringApplication.run(SprintbootDemoApplication.class, args);
     }
 
---
+++ src/main/java/com/yifeng/springboot/sprintbootdemo/dao/UserDao.java
@@ -0,0 +1,42 @@
+package com.yifeng.springboot.sprintbootdemo.dao;
+
+import com.yifeng.springboot.sprintbootdemo.entity.User;
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * @Description:CRUD操作
+ */
+
+public interface UserDao {
+    /**
+     *
+     * @Description:返回数据列表
+     * @return
+     */
+    List<User> findAllUsers();
+
+
+    /**
+     * @Description:添加
+     * @param user
+     * @return
+     */
+    int insertUser (User user);
+
+
+    /**
+     * @Description:修改
+     * @return
+     */
+    int updUser(User user);
+
+    /**
+     * @Description:删除
+     * @param id
+     * @return
+     */
+    int delUser(Integer id);
+
+}
